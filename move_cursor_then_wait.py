f3f3ce53ea1f0ca4f11c80cb92431c01459cd544
michaelryan <michaeldryanii>
2018-03-21 11:52:50 -0400

    fixed some string formatting

--- move_cursor_then_wait.py
+++ move_cursor_then_wait.py
@@ -14,14 +14,14 @@ def move_cursor_and_wait(time_stop):
 		move_x = randint(0, width)
 		move_y = randint(0, height)
 		
-		pyautogui.moveTo(move_x, move_y, duration=0.25)
+		pyautogui.moveTo(move_x, move_y, duration = 0.25)
 		
 		# wait, minutes depending on passed parameter
 		time_to_wait = time_stop * 60  # this will be how many minutes between each move
-		print('Before: %s' % time.ctime())
+		print('\nBefore: %s' % time.ctime())
 		print('\nLocation: x: %d y: %d' % (move_x, move_y))
 		time.sleep(time_to_wait)
-		print('After: %s\n' % time.ctime())
+		print('\nAfter: %s' % time.ctime())
 		
 		
 move_cursor_and_wait(3)
